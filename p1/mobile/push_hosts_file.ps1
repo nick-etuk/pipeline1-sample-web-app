430e34644c496826f524becea669de7dc777122a
Nick Etuk <niet2@hscic.gov.uk>
2026-05-26 13:02:00 +0100

    Updated push_hosts_file

--- p1/mobile/push_hosts_file.ps1
+++ p1/mobile/push_hosts_file.ps1
@@ -1,5 +1,15 @@
-function PushHostsFile {
+function push_hosts_file {
+    $script_dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
     adb root
     adb -s emulator-$ANDROID_EMULATOR_PORT remount
-    adb -s emulator-$ANDROID_EMULATOR_PORT push "$P1_ROOT_WIN/lib/conf/android-hosts.txt" /system/etc/hosts
+    adb reboot
+
+    # remount after reboot
+    adb root
+    adb -s emulator-$ANDROID_EMULATOR_PORT remount
+    adb -s emulator-$ANDROID_EMULATOR_PORT push "$script_dir/android-hosts.txt" /system/etc/hosts
+    # cd C:\Users\Nick\.pipeline1\remotes\pipeline1-sample-web-app\p1\mobile
+    # adb -s emulator-$ANDROID_EMULATOR_PORT push "android-hosts.txt" /system/etc/hosts
 }
+push_hosts_file
+
--- p1/mobile/start_emulator.ps1
+++ p1/mobile/start_emulator.ps1
@@ -1,8 +1,19 @@
-function Start-Emulator {
+function wait_for_emulator {
+    while ($true) {
+        $output = adb shell getprop sys.boot_completed 2>&1
+        if ($output -match "1") {
+            break
+        }
+        Start-Sleep -Seconds 1
+    }
+}
+
+function start_emulator {
     # No need to support ios emulator in Windows
 
-    # Start-Process pwsh -ArgumentList "-NoExit", "-c", "`$Host.UI.RawUI.BackgroundColor = 'DarkBlue'; Clear-Host; $PSScriptroot\start-emulator.ps1"
-    $ANDROID_ANDROID_DEFAULT_DEVICE="Pixel_5_API_34"
-    emulator -avd $ANDROID_ANDROID_DEFAULT_DEVICE -no-snapshot-load -writable-system
-    Wait-For-Emulator
+    # Start-Process pwsh -ArgumentList "-NoExit", "-c", "`$Host.UI.RawUI.BackgroundColor = 'DarkBlue'; Clear-Host; $PSScriptroot\start_emulator.ps1"
+    
+    emulator -avd $ANDROID_DEFAULT_DEVICE -no-snapshot-load -writable-system
+    wait_for_emulator
 }
+start_emulator
