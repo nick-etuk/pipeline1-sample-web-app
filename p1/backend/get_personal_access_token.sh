bb1e160e59dd19c02f594eaabfe9774bd8282948
Nick Etuk <nick_etuk@hotmail.com>
2026-07-19 15:55:09 +0100

    Copy pre-configured files in package_feed.sh

--- p1/backend/get_personal_access_token.sh
+++ p1/backend/get_personal_access_token.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
+copy_source_file() {
+    local filename="$1"
+    local fullpath
+
+    fullpath="$WORKING_DIR_ONEDRIVE/$filename"
+    if [ -f "$fullpath" ]; then
+        info "$filename found in OneDrive, using it."
+        cp "$fullpath" "$WORKING_DIR"
+    fi
+}
 get_personal_access_token() {
     local pat_file
     local pat
     local option
     local short_code
     
+    short_code=$(get_context 'short_code')
+    if [ -z "$short_code" ] && [ "$VM" = 'wsl' ]; then
+        short_code=$P1_USER_WIN
+    fi
+
+    while [ -z "$short_code" ]; do
+        read -rp "Enter your HSCIC short code, for example niet2: " short_code
+    done
+
+    set_context short_code "$short_code"    
     if [ -n "$WORKING_DIR_ONEDRIVE" ]; then
-        pat_file="$WORKING_DIR_ONEDRIVE/.pat"
-        if [ -f "$pat_file" ]; then
-            pat=$(cat "$pat_file")
-            if [ -n "$pat" ]; then
-                echo "Personal Access Token found in OneDrive, using it."
-                echo "$pat" > "$WORKING_DIR/.pat"
-                return
-            fi
-        fi
+        copy_source_file ".pat"
+        return
     fi
 
     info 'No Personal Access Token found.'
@@ -27,12 +40,5 @@ get_personal_access_token() {
         read -rp "Personal access token: " pat
     done
     echo "$pat" > "$WORKING_DIR/.pat"
-    
-    short_code=$(get_context 'short_code')
-    [ -z "$short_code" ] && [ "$VM" = 'wsl' ] && short_code=$P1_USER_WIN
-    while [ -z "$short_code" ]; do
-        read -rp "Enter your HSCIC short code, for example niet2: " short_code
-    done
-    set_context short_code "$short_code"
 }
 get_personal_access_token
