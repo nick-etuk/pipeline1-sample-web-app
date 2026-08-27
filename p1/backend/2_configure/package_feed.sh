e52b6c3c3ee4b60f46a5e49aaf4d8833cae73076
Nick Etuk <nick.etuk2@nhs.net>
2026-08-27 18:54:15 +0100

    Fix package_feed.sh

--- p1/backend/2_configure/package_feed.sh
+++ p1/backend/2_configure/package_feed.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 fix_nuget_filename() {
-  local buildscripts_dir
+  local nuget_dir
   local bad_filename
   local good_filename
 
-  buildscripts_dir="$REPO_DIR"/nhsapp/buildscripts
-  bad_filename="$buildscripts_dir"/package_feeds/Nuget.config
-  good_filename="$buildscripts_dir"/package_feeds/NuGet.Config
+  # buildscripts_dir="$REPO_DIR"/nhsapp/buildscripts
+  nuget_dir="$HOME/.nuget/NuGet"
+
+  bad_filename="$nuget_dir/Nuget.config"
+  good_filename="$nuget_dir/NuGet.Config"
 
   if [ -f "$bad_filename" ]; then
     mv "$bad_filename" "$good_filename"
-    warn "Fixed capitalisation of NuGet.Config in $buildscripts_dir/package_feeds"
+    warn "Fixed capitalisation of NuGet.Config in $nuget_dir"
   fi
 }
 
-preconfigured_files() {
+has_preconfigured_files() {
   [ -z "$WORKING_DIR_ONEDRIVE" ] && return 1
 
   if [ -f "$WORKING_DIR_ONEDRIVE/.pat" ] && [ "$WORKING_DIR_ONEDRIVE/.pat" -nt "$WORKING_DIR/.pat" ]; then
@@ -34,25 +36,45 @@ preconfigured_files() {
   return 1
 }
 
+save_preconfigured_files() {
+  if [ -z "$WORKING_DIR_ONEDRIVE" ]; then
+    warn "WORKING_DIR_ONEDRIVE is not set. Cannot save preconfigured files."
+    return
+  fi
+
+  info "Saving preconfigured files to $WORKING_DIR_ONEDRIVE"
+  cp "$HOME/.npmrc" "$WORKING_DIR_ONEDRIVE/.npmrc"
+  cp "$HOME/.nuget/NuGet/NuGet.Config" "$WORKING_DIR_ONEDRIVE/NuGet.Config"
+  cp "$HOME/.m2/settings.xml" "$WORKING_DIR_ONEDRIVE/settings.xml"
+}
+
 configure_package_feed() {
   local buildscripts_dir
   local email
   local pat
   
-  if preconfigured_files; then
+  if has_preconfigured_files; then
     info "Package feed configured from existing files."
-    return 0
+    return
   fi
 
   buildscripts_dir="$REPO_DIR"/nhsapp/buildscripts
-  fix_nuget_filename
+  # fix_nuget_filename
 
-  email="$P1_USER_WIN@hscic.gov.uk"
+  if [ "$MY_OS" = 'macos' ]; then
+    email="$P1_USER_UNIX@hscic.gov.uk"
+  else
+    email="$P1_USER_WIN@hscic.gov.uk"
+  fi
   pat=$(cat "$WORKING_DIR/.pat")
 
+  info 'Configuring package feed'
+  info "email: $email"
+  info "PAT: $pat"
   "$buildscripts_dir"/configure_package_feed.sh "$pat" "$email"
 
   fix_nuget_filename
+  save_preconfigured_files
 }
 
 configure_package_feed
